e4426c1596bf9d1486f372014a7c2fbb31757984
Divz1810 <59832425+Divz1810@users.noreply.github.com>
2021-06-21 23:06:10 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//var React= require("react");
+//var ReactDOM = require("react-dom");
+import React from "react"; //mordern ES6 method to import.
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!!</h1>
+    <p> Hey again!</p>
+  </div>,
+  document.getElementById("root")
+);
