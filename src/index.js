4abc0807f0d55f564c6dc32612a062be0c22a364
Divz1810 <59832425+Divz1810@users.noreply.github.com>
2021-06-22 15:11:48 +0530

    styling learnt

--- src/index.js
+++ src/index.js
@@ -11,29 +11,30 @@ import ReactDOM from "react-dom";
 //   document.getElementById("root")
 // );
 
-ReactDOM.render(
-  <div>
-    <h1>My Skills</h1>
-    <ul>
-      <img
-        className="skill-img"
-        src="https://tse4.mm.bing.net/th?id=OIP.SADZ9CkWXMQVH1HpxamqdAHaE4&pid=Api&P=0&w=236&h=156"
-        alt="node"
-      ></img>
-      <img
-        className="skill-img"
-        src="https://tse4.mm.bing.net/th?id=OIP.s4-3twOVbpbGEvYDHcopAwHaD8&pid=Api&P=0&w=314&h=168"
-        alt="react"
-      ></img>
-      <img
-        className="skill-img"
-        src="https://tse3.mm.bing.net/th?id=OIP.3of4QJ65oynrDrNpgHYXTgHaD4&pid=Api&P=0&w=323&h=170"
-        alt="js"
-      ></img>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+//***ADDING IMG ***
+// ReactDOM.render(
+//   <div>
+//     <h1>My Skills</h1>
+//     <ul>
+//       <img
+//         className="skill-img"
+//         src="https://tse4.mm.bing.net/th?id=OIP.SADZ9CkWXMQVH1HpxamqdAHaE4&pid=Api&P=0&w=236&h=156"
+//         alt="node"
+//       ></img>
+//       <img
+//         className="skill-img"
+//         src="https://tse4.mm.bing.net/th?id=OIP.s4-3twOVbpbGEvYDHcopAwHaD8&pid=Api&P=0&w=314&h=168"
+//         alt="react"
+//       ></img>
+//       <img
+//         className="skill-img"
+//         src="https://tse3.mm.bing.net/th?id=OIP.3of4QJ65oynrDrNpgHYXTgHaD4&pid=Api&P=0&w=323&h=170"
+//         alt="js"
+//       ></img>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
 
 //******ADDING JS INSIDE HTML*****
 // const name = "Divyanshi";
@@ -62,3 +63,35 @@ ReactDOM.render(
 //   </div>,
 //   document.getElementById("root")
 // );
+
+//*****INLINE-CSS STYLING*****
+// const name = "Divyanshi";
+// const lastName = "Mishra";
+// const number = "29";
+// ReactDOM.render(
+//    <div>
+//      <h1 style={{color:"red"}}>
+//      Hello I am {name} {lastName}!
+//     </h1>
+//     <p>My lucky number is {number}</p>
+//    </div>,
+//    document.getElementById("root")
+//  );
+
+//****INTERNAL CSS STYLING*****
+const name = "Divyanshi";
+const lastName = "Mishra";
+const customStyle = {
+  color: "red",
+  fontSize: "30px",
+  border: "1px solid black"
+};
+customStyle.color = "blue"; //changes color
+ReactDOM.render(
+  <div>
+    <h1 style={customStyle}>
+      Hello I am {name} {lastName}!
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
